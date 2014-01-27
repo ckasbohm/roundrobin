1587d6e22032acc39a413124309eec4ef8494a4e
ckasbohm <ckasbohm@accesso.com>
2014-01-27 12:57:52 -0500

    real commit

---
+++ Platformer/Platformer/Animation.cs
@@ -0,0 +1,114 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// Animation.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RoundRobin
+{
+    /// <summary>
+    /// Represents an animated texture.
+    /// </summary>
+    /// <remarks>
+    /// Currently, this class assumes that each frame of animation is
+    /// as wide as each animation is tall. The number of frames in the
+    /// animation are inferred from this.
+    /// </remarks>
+    class Animation
+    {
+        /// <summary>
+        /// All frames in the animation arranged horizontally.
+        /// </summary>
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
+        Texture2D texture;
+
+        /// <summary>
+        /// Duration of time to show each frame.
+        /// </summary>
+        public float FrameTime
+        {
+            get { return frameTime; }
+        }
+        float frameTime;
+
+
+        public int Rows
+        {
+            get { return rows; }
+        }
+        int rows;
+
+        public int Columns
+        {
+            get { return columns; }
+        }
+        int columns;
+
+        /// <summary>
+        /// When the end of the animation is reached, should it
+        /// continue playing from the beginning?
+        /// </summary>
+        public bool IsLooping
+        {
+            get { return isLooping; }
+        }
+        bool isLooping;
+
+        /// <summary>
+        /// Gets the number of frames in the animation.
+        /// </summary>
+        public int FrameCount
+        {
+            get { return frameCount; }
+        }
+        int frameCount;
+
+
+        /// <summary>
+        /// Gets the width of a frame in the animation.
+        /// </summary>
+        public int FrameWidth
+        {
+            // Assume square frames.
+            get { return frameWidth; }
+        }
+        int frameWidth;
+        /// <summary>
+        /// Gets the height of a frame in the animation.
+        /// </summary>
+        public int FrameHeight
+        {
+            get { return frameHeight; }
+        }
+        int frameHeight;
+
+
+
+        /// <summary>
+        /// Constructors a new animation.
+        /// </summary>        
+        public Animation(Texture2D texture, float frameTime, bool isLooping,int frameCount,int frameWidth, int frameHeight, int rows, int columns)
+        {
+            this.texture = texture;
+            this.frameTime = frameTime;
+            this.isLooping = isLooping;
+            this.frameCount = frameCount;
+            this.frameWidth = frameWidth;
+            this.frameHeight = frameHeight;
+            this.rows = rows;
+            this.columns = columns;
+
+           
+
+        }
+    }
+}
